73ce58fd82382c28efcd513fe8a95a3a37589bd5
Mohamed Asem <105158007+MohamedAsem532@users.noreply.github.com>
2026-01-12 15:52:56 +0200

    Smoother filter and animation file
    -- Modified js script
    -- added animation.css

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 const filterItems = document.querySelectorAll(".filter-menu li");
 const menuCards = document.querySelectorAll(".menu-card");
 
-filterItems.forEach(item => {
+filterItems.forEach((item) => {
   item.addEventListener("click", () => {
-
     // active class
-    filterItems.forEach(li => li.classList.remove("active"));
+    filterItems.forEach((li) => li.classList.remove("active"));
     item.classList.add("active");
-    
+
     const filter = item.dataset.filter;
 
-    menuCards.forEach(card => {
+    menuCards.forEach((card) => {
       const category = card.dataset.category;
 
+      card.classList.remove("show");
+      card.classList.add("hide");
+
       if (filter === "all" || filter === category) {
-        card.style.display = "block";
-      } else {
-        card.style.display = "none";
+        card.classList.remove("hide");
+
+        // Force reflow to restart animation
+        void card.offsetWidth;
+
+        card.classList.add("show");
       }
     });
   });
